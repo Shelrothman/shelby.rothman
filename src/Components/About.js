27efc8f3ee6c46a067fa03cf58568810dc190dbe
Q118 <shelbyfish91@gmail.com>
2020-08-13 16:53:14 -0700

    updated skills

--- src/Components/About.js
+++ src/Components/About.js
@@ -22,7 +22,7 @@ class About extends Component {
       <section id="about">
       <div className="row">
          <div className="three columns">
-            <Image height= {250} width= {250} className="profile-pic" src={profilepic} alt="Tim Baker Profile Pic" />
+            <Image height= {250} width= {250} className="profile-pic" src={profilepic} alt="Shelby Profile Pic" />
          </div>
          <div className="nine columns main-col">
             <h2>About Me</h2>
